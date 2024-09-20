777e95f644e69afa8f17b0116c78e81974b0d811
Precious <precious.soriano@ynov.com>
2024-09-20 18:51:03 +0200

    small fux

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // Middleware pour analyser les corps de requête
-const morgan = require('morgan'); // Middleware pour enregistrer les requêtes HTTP dans la console
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const path = require('path');
 const sessionMiddleware = require('./middlewares/sessionMiddleware');
 const authRoutes = require('./routes/authRoutes');
@@ -10,27 +10,20 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware pour analyser les corps de requête
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Middleware pour gérer les sessions
 app.use(sessionMiddleware);
 
-// Middleware pour les logs de requêtes HTTP dans la console
 app.use(morgan('dev'));
 
-// Middleware pour servir des fichiers statiques
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 
 app.use('/', authRoutes);
 
-// Activez CORS pour toutes les routes
 app.use(cors());
 
-// Démarrage du serveur Express
-const PORT = process.env.PORT || 3006; // Utilisation du port spécifié dans les variables d'environnement ou le port 3006 par défaut
+const PORT = process.env.PORT || 3006; 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
